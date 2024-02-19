de10309a3396392d5f416c84c3ee7e09af2c5ea8
luqmanmatloob <93182189+luqmanmatloob@users.noreply.github.com>
2024-02-19 13:48:24 +0500

    updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,68 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import PrimaryBtn from './primarybtn'
+import { SiHtml5 } from "react-icons/si";
+import { FaCss3Alt } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { BiLogoTypescript } from "react-icons/bi";
+import { PiFileSqlFill } from "react-icons/pi";
+import { FaBootstrap } from "react-icons/fa";
+import { SiTailwindcss } from "react-icons/si";
+import { TbBrandNodejs } from "react-icons/tb";
+import { FaReact } from "react-icons/fa";
+import { SiNextdotjs } from "react-icons/si";
+import { SiMicrosoftsqlserver } from "react-icons/si";
+import { SiMongodb } from "react-icons/si";
+import { FaGitAlt } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaFigma } from "react-icons/fa";
+import { SiAdobexd } from "react-icons/si";
+import { SiCanva } from "react-icons/si";
+
 const About = () => {
+  const iconArray = [
+    // { icon: <SiHtml5 className=" text-3xl md:text-6xl" />, label: 'HTML' },
+    // { icon: <FaCss3Alt className="text-3xl md:text-6xl" />, label: 'CSS' },
+    // { icon: <IoLogoJavascript className=" text-3xl md:text-6xl" />, label: 'JavaScript' },
+    // { icon: <BiLogoTypescript className=" text-3xl md:text-6xl" />, label: 'TypeScript' },
+    // { icon: <PiFileSqlFill className=" text-3xl md:text-6xl" />, label: 'SQL' },
+    // { icon: <FaBootstrap className=" text-3xl md:text-6xl" />, label: 'Bootstrap' },
+    // { icon: <SiTailwindcss className=" text-3xl md:text-6xl" />, label: 'TailwindCSS' },
+    // { icon: <TbBrandNodejs className=" text-3xl md:text-6xl" />, label: 'NodeJS' },
+    // { icon: <FaReact className=" text-3xl md:text-6xl" />, label: 'React' },
+    // { icon: <SiNextdotjs className=" text-3xl md:text-6xl" />, label: 'NextJSD' },
+    // { icon: <SiMicrosoftsqlserver className=" text-3xl md:text-6xl" />, label: 'Sql Server' },
+    // { icon: <SiMongodb className=" text-3xl md:text-6xl" />, label: 'Mongodb' },
+    // { icon: <FaGitAlt className=" text-3xl md:text-6xl" />, label: 'Git' },
+    // { icon: <FaGithub className=" text-3xl md:text-6xl" />, label: 'Github' },
+    // { icon: <FaFigma className=" text-3xl md:text-6xl" />, label: 'Figma' },
+    // { icon: <SiAdobexd className=" text-3xl md:text-6xl" />, label: 'XD' },
+    // { icon: <SiCanva className=" text-3xl md:text-6xl" />, label: 'Canva' },
+    
+
+
+      { icon: <SiHtml5 className="text-red-500 text-3xl md:text-6xl" />, label: 'HTML' },
+      { icon: <FaCss3Alt className="text-blue-500 text-3xl md:text-6xl" />, label: 'CSS' },
+      { icon: <IoLogoJavascript className="text-yellow-500 text-3xl md:text-6xl" />, label: 'JavaScript' },
+      { icon: <BiLogoTypescript className="text-blue-600 text-3xl md:text-6xl" />, label: 'TypeScript' },
+      { icon: <PiFileSqlFill className="text-green-500 text-3xl md:text-6xl" />, label: 'SQL' },
+      { icon: <FaBootstrap className="text-purple-500 text-3xl md:text-6xl" />, label: 'Bootstrap' },
+      { icon: <SiTailwindcss className="text-teal-500 text-3xl md:text-6xl" />, label: 'TailwindCSS' },
+      { icon: <TbBrandNodejs className="text-green-600 text-3xl md:text-6xl" />, label: 'NodeJS' },
+      { icon: <FaReact className="text-blue-400 text-3xl md:text-6xl" />, label: 'React' },
+      { icon: <SiNextdotjs className="text-black text-3xl md:text-6xl" />, label: 'NextJSD' },
+      { icon: <SiMicrosoftsqlserver className="text-blue-800 text-3xl md:text-6xl" />, label: 'Sql Server' },
+      { icon: <SiMongodb className="text-green-600 text-3xl md:text-6xl" />, label: 'Mongodb' },
+      { icon: <FaGitAlt className="text-red-700 text-3xl md:text-6xl" />, label: 'Git' },
+      { icon: <FaGithub className="text-black text-3xl md:text-6xl" />, label: 'Github' },
+      { icon: <FaFigma className="text-purple-600 text-3xl md:text-6xl" />, label: 'Figma' },
+      { icon: <SiAdobexd className="text-red-800 text-3xl md:text-6xl" />, label: 'XD' },
+      { icon: <SiCanva className="text-blue-700 text-3xl md:text-6xl" />, label: 'Canva' }
+    
+  ];
+
+
+
   return (
     <div className=''>
       <div className='mt-10 mb-10 container flex flex-col items-center mx-auto '>
@@ -35,7 +96,32 @@ const About = () => {
           My Skills
         </p>
 
+
+
+
+
         <div className='flex flex-wrap gap-8 my-5 justify-center items-center mx-3 max-w-3xl'>
+          {iconArray.map((item, index) => (
+            <div key={index} className='text-center'>
+              <div className='flex justify-center items-center'>
+                              {item.icon}
+
+              </div>
+              <p className='text-center text-sm mt-1'>{item.label}</p>
+            </div>
+          ))}
+        </div>
+
+
+
+
+        {/* <div className='flex flex-wrap gap-8 my-5 justify-center items-center mx-3 max-w-3xl'>
+
+
+
+
+
+
           <div className=''>
             <Image
               src='/img/html.png'
@@ -46,6 +132,10 @@ const About = () => {
             />
             <p className='text-center text-sm mt-1'>HTML</p>
           </div>
+
+
+
+
 
           <div className=''>
             <Image
@@ -164,7 +254,10 @@ const About = () => {
             />
             <p className='text-center text-sm mt-1'>Canva</p>
           </div>
-        </div>
+        </div> */}
+
+
+
       </div>
 
       <div className='  border-[#e2e2e2]   max-w-[700px]  mx-auto '>
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,11 +6,11 @@ import Link from 'next/link'
 
 const Portfolio = () => {
     return (
-        <section  className=''>
-        <div className='mb-14' >
+        <section className=''>
+            <div className='mb-14' >
 
 
-            {/* 
+                {/* 
             <div className='bg-[#EAEAEA] py-16 px-10 '>
                 <div className='container md:flex items-center mx-auto max-w-[1200px]  '>
                     <p className='font-extrabold text-2xl md:border-r-4 border-[#b4b4b4] md:mr-6 md:pr-6 mb-10 md:mb-0 '>
@@ -25,45 +25,63 @@ const Portfolio = () => {
 
 
 
-            <div className='mt-32 mb-10 container flex flex-col items-center mx-auto '>
+                <div className='mt-32 mb-10 container flex flex-col items-center mx-auto '>
 
-                <p className='text-center bold font-extrabold text-4xl'>
-                    Portfolio
-                </p>
-                <p className='max-w-[80%] text-center mx-auto mt-10 my-10'>
-                </p>
+                    <p className='text-center bold font-extrabold text-4xl'>
+                        Portfolio
+                    </p>
+                    <p className='max-w-[80%] text-center mx-auto mt-10 my-10'>
+                    </p>
 
 
-                <div className='flex flex-wrap gap-10 justify-center px-5'>
-                    
-                   
-                    <div className='rounded-md border-2 border-[#cacaca] p-5 drop-shadow-xl shadow-xl '>
-                        <Image
-                            src="/Portfolio/portfolio11.png"
-                            width='400'
-                            height='250'
-                            alt=""
-                            className='w-[%] h-[%]'
-                        />
-                        <div className='my-5'>
-                            <Link href="https://todoapp-luqman.vercel.app/"target='_blank'><PrimaryBtn text="Live Demo" /></Link>
+                    <div className='flex flex-wrap gap-10 justify-center px-5 max-w-7xl'>
+
+
+                        <div className='rounded-md border-2 border-[#cacaca] p-5 drop-shadow-xl shadow-xl '>
+                            <Image
+                                src="/Portfolio/portfolio11.png"
+                                width='400'
+                                height='250'
+                                alt=""
+                                className='w-[%] h-[%]'
+                            />
+                            <div className='my-5'>
+                                <Link href="https://todoapp-luqman.vercel.app/" target='_blank'><PrimaryBtn text="Live Demo" /></Link>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className='rounded-md border-2 border-[#cacaca] p-5 drop-shadow-xl shadow-xl '>
-                        <Image
-                            src="/Portfolio/portfolio7.jpg"
-                            width='400'
-                            height='250'
-                            alt=""
-                            className='w-[%] h-[%]'
-                        />
-                        <div className='my-5'>
-                            <Link href="https://fitnesshub.luqmanmatloob.vercel.app/ "target='_blank'><PrimaryBtn text="Live Demo" /></Link>
+
+                        <div className='rounded-md border-2 border-[#cacaca] p-5 drop-shadow-xl shadow-xl '>
+                            <Image
+                                src="/Portfolio/portfolio12.png"
+                                width='400'
+                                height='250'
+                                alt=""
+                                className='w-[%] h-[%]'
+                            />
+                            <div className='my-5'>
+                                <Link href="https://travelsite-luqman.vercel.app/" target='_blank'><PrimaryBtn text="Live Demo" /></Link>
+                            </div>
                         </div>
-                    </div>
 
 
+                        <div className='rounded-md border-2 border-[#cacaca] p-5 drop-shadow-xl shadow-xl '>
+                            <Image
+                                src="/Portfolio/portfolio7.jpg"
+                                width='400'
+                                height='250'
+                                alt=""
+                                className='w-[%] h-[%]'
+                            />
+                            <div className='my-5'>
+                                <Link href="https://fitnesshub.luqmanmatloob.vercel.app/ " target='_blank'><PrimaryBtn text="Live Demo" /></Link>
+                            </div>
+                        </div>
+
+
+
+
+                        {/* 
                     <div className='rounded-md border-2 border-[#cacaca] p-5 drop-shadow-xl shadow-xl '>
                         <Image
                             src="/Portfolio/portfolio8.jpeg"
@@ -75,40 +93,40 @@ const Portfolio = () => {
                         <div className='my-5'>
                             <Link href="https://food.luqmanmatloob.vercel.app/ " target='_blank'><PrimaryBtn text="Live Demo" /></Link>
                         </div>
-                    </div>
+                    </div> */}
 
 
-                    <div className='rounded-md border-2 border-[#cacaca] p-5 drop-shadow-xl shadow-xl '>
-                        <Image
-                            src="/Portfolio/portfolio9.jpeg"
-                            width='400'
-                            height='250'
-                            alt=""
-                            className='w-[%] h-[%]'
-                        />
-                        <div className='my-5'>
-                            <Link href="https://3-nextjs.vercel.app/" target='_blank' ><PrimaryBtn text="Live Demo" /></Link>
+                        <div className='rounded-md border-2 border-[#cacaca] p-5 drop-shadow-xl shadow-xl '>
+                            <Image
+                                src="/Portfolio/portfolio9.jpeg"
+                                width='400'
+                                height='250'
+                                alt=""
+                                className='w-[%] h-[%]'
+                            />
+                            <div className='my-5'>
+                                <Link href="https://3-nextjs.vercel.app/" target='_blank' ><PrimaryBtn text="Live Demo" /></Link>
+                            </div>
                         </div>
+
+
+
+
+
+
+
+
                     </div>
 
 
-
-
-
-
-
+                    <div className='mb-10 mt-16' id='contact_section'>
+                        <Link href="/portfolio">
+                            <PrimaryBtn text='View More' />
+                        </Link>
+                    </div>
 
                 </div>
-
-
-                <div className='mb-10 mt-16' id='contact_section'>
-                    <Link href="/portfolio">
-                        <PrimaryBtn text='View More' />
-                    </Link>
-                </div>
-
             </div>
-        </div>
         </section>
     )
 }
