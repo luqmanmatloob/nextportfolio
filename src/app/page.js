b29decd3fe76641d90dda3a72a0106f932595c3b
luqmanmatloob <93182189+luqmanmatloob@users.noreply.github.com>
2024-03-01 15:07:44 +0500

    About spell mistaek solved

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import Hero from 'src/components/hero'
 import Portfolio from 'src/components/Portfolio'
 import Contact from 'src/components/Contact'
-import About from 'src/components/about'
+import About from 'src/components/About'
 
 import { Metadata } from "next";
 
